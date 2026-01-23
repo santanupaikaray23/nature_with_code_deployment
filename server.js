d4ecda8ce09d4905e328196656840ffa33d2791f
santanupaikaray23 <santanupaikaray1996@gmail.com>
2026-01-23 14:08:39 +0530

    adding some changes

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require ('express');
 const path = require ('path');
 const app = express();
+const port = process.env.PORT||9999;
 
+app.use(express.static(path.join(__dirname,'build')));
 
-app.use(express.static(path.join(__dirname,'./dist/browser')));
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'build/index.html'))
 
-app.get('*',(req, res)=>{
-    res.sendFile(path.join(__dirname, './dist/browser/index.html'));
-})
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
 });
+app.listen(port);
