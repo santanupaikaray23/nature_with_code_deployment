137d08422af234e11205a87501b44f4d890cbd7f
santanupaikaray23 <santanupaikaray1996@gmail.com>
2026-03-22 11:08:17 +0530

    adding some changes

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-// const express = require ('express');
-// const path = require ('path');
-// const app = express();
-
-// app.use(express.static(path.join(__dirname,'./dist/browser')));
-
-// app.get('*',(req, res)=>{
-//     res.sendFile(path.join(__dirname, './dist/browser/index.html'));
-// })
-
-// const port = process.env.PORT || 3000;
-
-// app.listen(port, () => {
-//     console.log(`Server started on port ${port}`);
-// });
 const express = require ('express');
 const path = require ('path');
 const app = express();
-const port = process.env.PORT||9999;
 
-app.use(express.static(path.join(__dirname,'build')));
+app.use(express.static(path.join(__dirname,'./dist/browser')));
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'build/index.html'))
+app.get('*',(req, res)=>{
+    res.sendFile(path.join(__dirname, './dist/browser/index.html'));
+})
 
-});
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
+// const express = require ('express');
+// const path = require ('path');
+// const app = express();
+// const port = process.env.PORT||9999;
+
+// app.use(express.static(path.join(__dirname,'build')));
+
+// app.get('/',(req,res)=>{
+//     res.sendFile(path.join(__dirname,'build/index.html'))
+
+// });
+
+// app.listen(port, () => {
+//     console.log(`Server started on port ${port}`);
+// });
